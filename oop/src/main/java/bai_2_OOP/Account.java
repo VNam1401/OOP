162702e9377d6fc8c2191c0c1bfc2f7bd387ee7c
ADMIN <ADMIN@PC315>
2024-08-20 15:54:38 +0700

    03_bai_2

---
+++ oop/src/main/java/bai_2_OOP/Account.java
@@ -0,0 +1,66 @@
+package bai_2_OOP;
+
+public class Account {
+
+    public String id;
+    public String name;
+    public int balance;
+
+    public Account(String id, String name, int balance) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public void credit(int amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.print("Nap thanh cong " + amount + " Vao tai khoan " + name +".");
+        } else {
+            System.out.print("Tien la so nguyen duong.");
+        }
+    }
+
+    public void debit(int amount) {
+        if (amount > 0) {
+            if (amount <= balance) {
+                balance -= amount;
+                System.out.println("\n Thanh toan thanh cong " + amount + " Tu tai khoan cua " + name + ".");
+            } else {
+                System.out.println("Thanh toan that bai, so du khong du.");
+            }
+        } else {
+            System.out.println("Tien la so nguyen duong.");
+        }
+    }
+
+    public void transferTo(Account account, int amount) {
+        if (amount > 0) {
+            if (amount <= balance) {
+                balance -= amount;
+                account.credit(amount);
+                System.out.println("\n Chuyen thanh cong " + amount + " tai khoan cua " + name + " den tai khoan " + account.getName() + ".");
+            } else {
+                System.out.println("Chuyen tien khong thanh cong, so du khong du.");
+            }
+        } else {
+            System.out.println("So tien la so nguyen duong.");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public void display()  {
+        System.out.println("Tai khoan ID: " + id + ",Ten : " + name + ", So du: " + balance);
+    }
+}
---
+++ oop/src/main/java/bai_2_OOP/Main.java
@@ -0,0 +1,26 @@
+package bai_2_OOP;
+public class Main {
+    public static void main(String[] args) {
+          // Tao 2 tk account
+        Account myac = new Account("1", "Nguyen Van Nam", 1000);
+        Account frac = new Account("2", "Lieu Nhu Yen", 500);
+        // Gui tien vao tk1 (1200)
+        myac.credit(200);
+        // Rut tien tk1 (1050)
+        myac.debit(150);
+        // Chuyen tien tk1(950) sang tk2(600.)
+        myac.transferTo(frac, 100);
+
+        //    Gui tien vao tk2(1150)
+        frac.credit(300);
+        // Rut tien tk2 ()
+        frac.debit(50);
+        // Chuyen tien tk2 sang tk1
+        frac.transferTo(myac, 200);
+        // Hien thi thong tin tk
+        System.out.println("Thong tin tai khoan cua toi:");
+        myac.display();
+        System.out.println("\nThong tin tai khoan cua nguoi ban:");
+        frac.display();
+    }
+}
